431986e1e8245ffe98de45bcc53876d91802ea46
AmitVerma <amit.sitm@gmail.com>
2018-07-14 12:04:35 +0530

    cleaning

--- kittatrying1.py
+++ kittatrying1.py
@@ -1,6 +1,32 @@
-def fu(a):
-    return lambda inside : inside * a
+def iq_test(numbers):
 
-z = fu("a")
-x = fu("x")
-print(z(10)+x(10))
+    x = 0
+    y=numbers.split(" ")
+    o = 0
+    e = 0
+
+    while x < len(y):
+        if int(y[x])%2 == True:
+            o+=1
+        else:
+            e+=1
+        x+=1
+
+    z=0
+    if o==1:
+        while z < len(y):
+            if int(y[z])%2 == True:
+                return(z+1)
+                break
+            z+=1
+    elif e==1:
+        while z < len(y):
+            if int(y[z])%2 == False:
+                return(z+1)
+                break
+            z+=1
+    else:
+        return(0)
+
+    #o = [int(num) for num in y]
+# print(iq_test("1 2 3 4"))
